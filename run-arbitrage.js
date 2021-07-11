b8fc45fd7d1d8cc1de1636dbcfe9e3ce705c4913
Stephen Goudie <sgoudie@gmail.com>
2021-07-11 16:04:37 +1000

    Connect to ethereum using websocket, Emit event data when there is a new block and run async callback

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 const Web3 = require('web3');
-// Connect to ethereum using websocket
-const web3 = new Web3(
-    new Web3.providers.WebsocketProvider(process.env.INFRA_URL)
+const web3 = new Web3( // Connect to ethereum using websocket
+    new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
+
+
+// Emit event data when there is a new block and run async callback
+web3.eth.subscribe('newBlockHeaders')
+    .on('data', async block => {
+        console.log(`New block received. Block #${block.number}`);
+    })
+    .on('event', error => {
+        console.log(error);
+    });
